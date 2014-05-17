2df594768b33a074bb23494ea6249ca88bee68d1
U-HPE-150f\jmh <jmh@HPE-150f.(none)>
2014-05-17 17:10:38 +0900

    Indent cmd

---
+++ cmds/indent.js
@@ -0,0 +1,54 @@
+
+// Local
+var todo = require('../lib/todo.js');
+
+module.exports = function (program) {
+  'use strict';
+
+  program
+    .command('indent <index> [level]')
+    .description('Indents a range of tasks')
+    .action(function(index, level) {
+
+      if (level === undefined) {level = 1;}
+
+      todo
+        .options(program)
+        .load()
+        .indent(index, level)
+        .write();
+
+      if (!program.quiet) {
+        todo.list();
+
+        if (!!program.stats) {
+          todo.stats();
+        }
+      }
+
+    });
+
+  program
+    .command('unindent <index> [level]')
+    .description('Unindents a range of tasks')
+    .action(function(index, level) {
+
+      if (level === undefined) {level = -1;}
+
+      todo
+        .options(program)
+        .load()
+        .indent(index, -level)
+        .write();
+
+      if (!program.quiet) {
+        todo.list();
+
+        if (!!program.stats) {
+          todo.stats();
+        }
+      }
+
+    });
+
+};
